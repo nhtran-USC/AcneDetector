93b1ecddd22174117ff1dd02d750fa8cf6c12ef7
Nguyen Tran <nhtran@usc.edu>
2022-07-18 18:01:27 -0700

    new ios project

---
+++ AcneDetector/AcneDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AcneDetector
+//
+//  Created by Nguyen Tran on 7/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
